a757a640283dc34e594ef8c0b5d8a4ad60c81501
Roman-2013 <roma.pushkarev.1995@mail.ru>
2023-10-31 23:19:57 +0300

    31.10.2023

---
+++ 8 kyu.js
@@ -0,0 +1,29 @@
+//#1
+const  sumOfDifferences=(arr)=> {
+    if (!arr.length || arr.length === 1) {
+        return 0
+    }
+    arr.sort((a, b) => b - a)
+    let result = 0
+    for (let i = 0; i < arr.length - 1; i++) {
+        result += arr[i] - arr[i + 1]
+    }
+    return result
+}
+
+//#2
+const calculateAge = (yearOfBirth, year) => {
+    if (yearOfBirth < year) {
+        if (year - yearOfBirth === 1) {
+            return `You are ${year - yearOfBirth} year old.`
+        }
+        return `You are ${year - yearOfBirth} years old.`
+    }
+    if (yearOfBirth > year) {
+        if (yearOfBirth - year === 1) {
+            return `You will be born in ${yearOfBirth - year} year.`
+        }
+        return `You will be born in ${yearOfBirth - year} years.`
+    }
+    return 'You were born this very year!'
+}
