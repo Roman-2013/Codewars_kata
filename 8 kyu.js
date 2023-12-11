326d3c5368e47e4289dfca1909450ee3b2f4615d
Roman-2013 <roma.pushkarev.1995@mail.ru>
2023-12-12 00:00:10 +0300

    011.12.2023 (1 kata)

--- 8 kyu.js
+++ 8 kyu.js
@@ -164,3 +164,24 @@ const betterThanAverage=(classPoints, yourPoints)=> classPoints.reduce((acc,valu
 const  chromosomeCheck=(sperm)=> sperm.indexOf('Y')===-1
     ?"Congratulations! You're going to have a daughter."
     :"Congratulations! You're going to have a son."
+
+//#21
+function validateHello(greetings) {
+    if (greetings.toLowerCase().includes('hello')) {
+        return true
+    } else if (greetings.toLowerCase().includes('ciao')) {
+        return true
+    } else if (greetings.toLowerCase().includes('salut')) {
+        return true
+    } else if (greetings.toLowerCase().includes('hallo')) {
+        return true
+    } else if (greetings.toLowerCase().includes('hola')) {
+        return true
+    } else if (greetings.toLowerCase().includes('ahoj')) {
+        return true
+    } else if (greetings.toLowerCase().includes('czesc')) {
+        return true
+    } else {
+        return false
+    }
+}
--- currentFile.js
+++ currentFile.js
@@ -1,10 +1,36 @@
-const  chromosomeCheck=(sperm)=> sperm.indexOf('Y')===-1
-?"Congratulations! You're going to have a daughter."
-    :"Congratulations! You're going to have a son."
+function validateHello(greetings) {
+    if (greetings.toLowerCase().includes('hello')) {
+        return true
+    } else if (greetings.toLowerCase().includes('ciao')) {
+        return true
+    } else if (greetings.toLowerCase().includes('salut')) {
+        return true
+    } else if (greetings.toLowerCase().includes('hallo')) {
+        return true
+    } else if (greetings.toLowerCase().includes('hola')) {
+        return true
+    } else if (greetings.toLowerCase().includes('ahoj')) {
+        return true
+    } else if (greetings.toLowerCase().includes('czesc')) {
+        return true
+    } else {
+        return false
+    }
+
+}
+// if(greetings.includes('hallo ')){
+//     return true
+// }
+// if(greetings.includes('hola')){
+//     return true
+// }
+//
+// if(greetings.includes('czesc ')){
+//     debugger
+//     return true
+// }
 
 
 
-console.log(chromosomeCheck('XY'))//"Congratulations! You're going to have a son."
 
-let a ='XY'
-console.log(a.indexOf('v'))
+console.log(validateHello('czecsc'))//true
