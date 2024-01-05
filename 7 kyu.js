749f47a7eb2059642078290b5590a6fdc75fc25a
Roman-2013 <roma.pushkarev.1995@mail.ru>
2024-01-05 14:49:03 +0300

    05.01.2024 (1 kata)

--- 7 kyu.js
+++ 7 kyu.js
@@ -241,3 +241,17 @@ function nbDig(n, d) {
     return res
 
 }
+
+//#28
+function barista(coffees) {
+    const sort = coffees.sort((a, b) => a - b)
+    let accum = 0
+    for (let i = 1; i <= coffees.length; i++) {
+        if (i === 1) {
+            accum += sort[0]
+        } else {
+            accum += sort.slice(0, i).reduce((el, acc) => el + acc, 0) + (2 * (i - 1))
+        }
+    }
+    return accum
+}
--- currentFile.js
+++ currentFile.js
@@ -1,11 +1,16 @@
-function nbDig(n, d) {
-let res=0
-    for (let i = 0; i <=n ; i++) {
-let number=i**2
-        number.toString().split('').map(el=>Number(el)===d?res+=1:'')
+function barista(coffees) {
+    const sort = coffees.sort((a, b) => a - b)
+    let accum = 0
+    for (let i = 1; i <= coffees.length; i++) {
+        if (i === 1) {
+            accum += sort[0]
+        } else {
+            accum += sort.slice(0, i).reduce((el, acc) => el + acc, 0) + (2 * (i - 1))
+        }
     }
-return res
-
+    return accum
 }
 
-console.log(nbDig(5750, 0))//4700
+
+console.log(barista([5, 4, 3, 2, 1]))//55
+//2,3,4
